32abce4ee383591495afffbad5ed584105580b1f
ChenLiefang <57752347+ChenLiefang@users.noreply.github.com>
2020-12-23 18:35:01 +0800

    Add files via upload

---
+++ vote-fe/build/precache-manifest.ce8b6a2914c3892213705c7a53ca6fce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15302548a3a6baaf97988a908122a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36cb0a3e41d4845ce698",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "1d9d14089908a99f3e25",
+    "url": "/static/css/main.286090d3.chunk.css"
+  },
+  {
+    "revision": "36cb0a3e41d4845ce698",
+    "url": "/static/js/2.ce262e36.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.ce262e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9d14089908a99f3e25",
+    "url": "/static/js/main.5f23c295.chunk.js"
+  },
+  {
+    "revision": "a2d8c5043375827e22c3",
+    "url": "/static/js/runtime-main.9e194709.js"
+  },
+  {
+    "revision": "a5fbd574217fd9c1cd10c12801f8f647",
+    "url": "/static/media/danxuan.a5fbd574.jpg"
+  }
+]);
